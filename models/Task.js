b74e608eebbc3bed480f2cb8c500ab4f46314a1d
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-06-19 14:25:47 +0700

    change schemas

--- models/Task.js
+++ models/Task.js
@@ -10,7 +10,7 @@ const taskSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    maxlength: 200,
+    maxlength: 100,
   },
   description: {
     type: String,
