4a2ef71ead2c6522c412098219bdba503564ae8b
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-07-07 14:57:20 +0700

    optimise after merge

--- utils/aiUtils.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-const generateCrypto = (query) => {
-  const standardQuery = query.toLowerCase().trim()
-  return crypto.createHash('sha256').update(standardQuery).digest('hex')
-};
-
-module.exports = { generateCrypto };
