61002ea00997c0f41011371dc3abc482de9d8d7b
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-07-21 18:35:54 +0700

    update: add information for pagination response

--- src/controllers/user/searchUser.ts
+++ src/controllers/user/searchUser.ts
@@ -6,6 +6,12 @@ import User, { IUserDocument } from "../../models/User";
 import { sanitizeUserQuery } from "../../utils/sanitizeQuery";
 import { omit } from "../../utils/manipulate";
 
+export const buildResLimit = (data: any[], limit = 31, offset = 0, dataName?: string) => {
+  const isNext = data.length > limit - 1;
+  const nextOffset = isNext ? offset + limit - 1 : null;
+  return { isNext, nextOffset, [dataName || "data"]: data.slice(0, -1) };
+};
+
 export const searchUser = async (req: Request, res: Response) => {
   try {
     const { query, offset } = req.query;
@@ -13,17 +19,19 @@ export const searchUser = async (req: Request, res: Response) => {
       resMissingFields(res, "Query, Offset");
       return;
     }
+    const limit = 31;
+    const skip = parseInt(offset.toString()) || 0;
 
     const profileFound = (await findAndSanitize(
       User,
       { $or: [{ username: { $regex: query, $options: "i" } }, { fullName: { $regex: query, $options: "i" } }] },
-      { returnArray: true, options: { limit: 30, skip: parseInt(offset.toString()) || 0 } }
+      { returnArray: true, options: { limit, skip } }
     )) as IUserDocument[];
     const sanitizedProfile = profileFound?.map((profil) => {
       const partialProfile = omit(profil, ["goalsCompleted", "level", "points", "tasksCompleted"]);
       return sanitizeUserQuery(partialProfile as IUserDocument, { isGuest: true, deleteGoals: true });
     });
-    res.json(sanitizedProfile);
+    res.json(buildResLimit(sanitizedProfile, limit, skip));
   } catch (err) {
     handleError(err, res);
   }
